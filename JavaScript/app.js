db6611d89a1b513ab6bb9a30de6aa7e2530f92c3
Mike Bishop <mbishop@evequefou.be>
2022-05-18 21:51:08 -0400

    Add comments

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -42,18 +42,30 @@ const orders = [
 
 function numToString(num) {
     if (num in numLookup) {
+        // Cover 0-19 and multiples of 10 up to 90 with a straight lookup
         return numLookup[num];
     } else if (num < 100) {
+        // Cover two-digit numbers that aren't multiples of 10
+        // with simple recursion (but could also be a lookup)
         let tens = Math.floor(num / 10) * 10;
         return `${numToString(tens)}-${numToString(num % 10)}`
     } else {
+        // Anything larger is composed of the other cases.
+        //
+        // Find out what our biggest named place value is...
         let order = Math.floor(Math.log10(num) / 3);
+        // ...and the number corresponding to that place value
         let base = Math.pow(10, 3*(order));
+
+        // Three-digit numbers and four-digit numbers that
+        // aren't multiples of 1000 need slightly different
+        // handling.
         if (num < 10000 && num % 1000 !== 0) {
             order = 0;
             base = 100;
         }
 
+        // Now build the number with recursive calls.
         let aboveBase = Math.floor(num / base);
         let string = `${numToString(aboveBase)} ${orders[order]}`;
         if (num % base !== 0) {
